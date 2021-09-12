ff13680baeab051063abb94f20fd0fb2355aef09
narendra <narendrakareli@yahoo.com>
2021-09-12 16:19:24 +0530

    Added SchedulerServices

--- src/main/java/com/narendra/testcontainer/dyb/repository/BooksRepository.java
+++ src/main/java/com/narendra/testcontainer/dyb/repository/BooksRepository.java
@@ -1,11 +1,13 @@
 package com.narendra.testcontainer.dyb.repository;
 
 import com.narendra.testcontainer.dyb.model.Books;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BooksRepository extends CrudRepository<Books, Integer>
 {
+    @Modifying
     @Query(value = "DELETE FROM BOOKS where (cre_ts + INTERVAL '2' MINUTE )< SYSTIMESTAMP",nativeQuery = true)
     int purgeRecords();
 }
--- src/main/java/com/narendra/testcontainer/dyb/services/SchedulerServices.java
+++ src/main/java/com/narendra/testcontainer/dyb/services/SchedulerServices.java
@@ -5,12 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
+@Transactional
 @Slf4j
 public class SchedulerServices {
-    @Autowired
-    BooksRepository booksRepository;
+     final BooksRepository booksRepository;
+
+    public SchedulerServices(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
 
     @Scheduled(fixedDelay = 120*1000L)
     public void purgeRecords() {
